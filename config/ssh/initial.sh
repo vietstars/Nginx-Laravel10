41b47f1f80417934902f091441b9382b3f973954
Việt Stars <vietstar.nt@gmail.com>
2023-07-13 16:50:26 +0700

    Update initial.sh

--- config/ssh/initial.sh
+++ config/ssh/initial.sh
@@ -1,6 +1,19 @@
 echo "**** Configure source ****"
 
 cp .env.example .env
-composer install
-npm install
 
+composer install
+
+composer require vietstars/code-style
+php artisan vendor:publish \
+  --tag=config-fixer \
+  --force
+
+composer require vietstars/logs-viewer
+php artisan vendor:publish \
+  --provider="Vietstars\LogsViewer\LogsViewerServiceProvider" \
+  --tag=views
+
+composer require vietstars/dev-debugger
+
+npm install
