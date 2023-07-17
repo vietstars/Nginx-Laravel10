437b39088436af444c08f9d8e86344742e678500
Việt Stars <vietstar.nt@gmail.com>
2023-07-17 14:45:11 +0700

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->environment(['production', 'staging', 'testing'])) {
+            URL::forceScheme('https');
+        }
+    }
+}
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,80 @@
+<?php return array (
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+  'vietstars/code-style' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Vietstars\\CodeStyle\\ServiceProvider',
+    ),
+  ),
+  'vietstars/dev-debugger' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Vietstars\\DevDebugger\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debug' => 'Vietstars\\DevDebugger\\Facades\\DevDebugger',
+    ),
+  ),
+  'vietstars/logs-viewer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Vietstars\\LogsViewer\\LogsViewerServiceProvider',
+    ),
+  ),
+);
