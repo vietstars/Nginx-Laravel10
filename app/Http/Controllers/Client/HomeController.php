5b37d3f9c1b9cf98ba4eca8e2b34d46eb48e9d71
Việt Stars <vietstar.nt@gmail.com>
2023-07-24 11:11:59 +0700

    Update HomeController.php

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
+  public function initApp()
+  {
+    return view('initial-app');
+  }
+
   public function welcome()
   {
     \Debug::error('Error! hello every one');
