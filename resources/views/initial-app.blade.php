1e1dcbf133849820f5f05509b8e6b8673060ed33
Việt Stars <vietstar.nt@gmail.com>
2023-07-24 10:19:40 +0700

    Add files via upload

---
+++ resources/views/initial-app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>How To Install React in Laravel 9 with Vite</title>
+
+    {{ vite_assets() }} 
+</head>
+<body class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+  <div id="root"></div>
+</body>
+</html>
