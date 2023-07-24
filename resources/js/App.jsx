f39bebfb50f6a2c8cb0f425fc7f419cadfeca6e3
Việt Stars <vietstar.nt@gmail.com>
2023-07-24 14:07:28 +0700

    Update App.jsx

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { 
   createRoot 
 } from 'react-dom/client';
-
-export default function App() {
-  return(
-    <h1 className="dark:text-white">
-      How To Install React in Laravel 10 with Vite
-    </h1>
-  );
-}
+import App from './scenes/welcome';
 
 const root = createRoot(document.getElementById('root'));
 
